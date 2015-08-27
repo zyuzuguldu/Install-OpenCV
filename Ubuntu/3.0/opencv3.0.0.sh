c456a8ee23afc15759da3d00b5dcd2371c9d9804
Mehmet Zahid Yüzügüldü <zyuzuguldu@gmail.com>
2015-08-27 11:59:54 +0300

    initial commit

--- Ubuntu/3.0/opencv3.0.0.sh
+++ Ubuntu/3.0/opencv3.0.0.sh
@@ -4,9 +4,9 @@ flag=1
 else
 flag=0
 fi
-echo "Installing OpenCV 2.4.5"
-mkdir OpenCV
-cd OpenCV
+echo "Installing OpenCV 3.0.0"
+mkdir ../OpenCV
+cd ../OpenCV
 echo "Removing any pre-installed ffmpeg and x264"
 sudo apt-get -y remove ffmpeg x264 libx264-dev
 echo "Installing Dependenices"
@@ -19,16 +19,26 @@ sudo apt-get -y install libtbb-dev
 sudo apt-get -y install libqt4-dev libgtk2.0-dev
 sudo apt-get -y install libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev
 sudo apt-get -y install x264 v4l-utils ffmpeg
-echo "Downloading OpenCV 2.4.5"
-wget -O OpenCV-2.4.5.tar.gz http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.5/opencv-2.4.5.tar.gz/download
-echo "Installing OpenCV 2.4.5"
-tar -xvf OpenCV-2.4.5.tar.gz
-cd OpenCV-2.4.5
+sudo apt-get -y install libgtk2.0-dev
+file="opencv-3.0.0.zip"
+if [ -f "$file" ]
+then
+	echo "$file found."
+else
+	echo "$file not found."
+	echo "Downloading $file"
+    	wget -O opencv-3.0.0.zip https://github.com/Itseez/opencv/archive/3.0.0.zip
+	wget -O opencv_contrib-3.0.0.zip https://github.com/Itseez/opencv_contrib/archive/3.0.0.zip
+fi
+echo "Installing OpenCV 3.0.0"
+unzip opencv-3.0.0.zip
+unzip opencv_contrib-3.0.0.zip
+cd opencv-3.0.0
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
-make -j4
+cmake -D BUILD_opencv_cvv=OFF -D BUILD_SHARED_LIBS=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-3.0.0/modules ..
+make -j8
 sudo make install
 sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
 sudo ldconfig
-echo "OpenCV 2.4.4 ready to be used"
+echo "OpenCV 3.0.0 ready to be used"
